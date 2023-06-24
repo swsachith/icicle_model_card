8ead82b8a2f1e23694dee2c2abb2818d658d2cdb
Sachith Withana <swsachith@gmail.com>
2023-06-24 18:35:12 -0400

    ading the schema json

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
-import glob
 
 setup(
     name='icicle_model_card',
-    version='0.13',
+    version='0.131',
     packages=['tests', 'icicle_model_card'],
+    package_data={'icicle_model_card': ['schema/schema.json']},
+    include_package_data=True,
     url='https://github.com/swsachith/icicle_model_card.git',
     license='BSD-3-Clause',
     author='Sachith Withana',
     author_email='swithana@iu.edu',
     description='ICICLE Model Card Tool',
-    data_files=glob.glob('schema/**'),
     install_requires=[
         'attrs~=23.1.0',
         'jsonschema~=4.17.3'
